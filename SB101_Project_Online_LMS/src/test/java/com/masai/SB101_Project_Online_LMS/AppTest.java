5806d9b54713e0c2905faee248f7158f9d228b52
Shivendra Kumar <123854927+shivendra-somr@users.noreply.github.com>
2023-07-19 23:35:33 +0530

    Day_1 task

---
+++ SB101_Project_Online_LMS/src/test/java/com/masai/SB101_Project_Online_LMS/AppTest.java
@@ -0,0 +1,20 @@
+package com.masai.SB101_Project_Online_LMS;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
